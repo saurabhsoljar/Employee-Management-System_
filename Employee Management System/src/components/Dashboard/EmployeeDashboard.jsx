ca9fa7eaa2a425999d4207a1c352af196d9fdef5
saurabh kumar <saurabhslite2@gmail.com>
2025-04-08 10:49:28 +0530

    create some file

--- Employee Management System/src/components/Dashboard/EmployeeDashboard.jsx
+++ Employee Management System/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from '../other/Header'
+import TaskListNumbers from '../other/TaskListNumbers'
+import TaskList from '../../TaskList/TaskList'
+
 
 const EmployeeDashboard = () => {
     return (
         <div className='p-10 bg-[#1c1c1c] h-screen'> 
-            <h1>EmployeeDashboard</h1>
+            <Header/>
+            <TaskListNumbers/>
+            <TaskList/>
         </div>
     )
 }
